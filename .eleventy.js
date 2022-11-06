3ffad7673b77101ca8b5d0e6fbfc67dab7ba52e4
vince falconi <vince@tattooed.dev>
2022-11-06 15:05:32 -0600

    polishing up some things
    - making project root path based on environment
    - fixing font weirdness. firefox was mad about quotes.
    - fixing safari details-summary styling
    -
    
    Signed-off-by: vince falconi <vince@tattooed.dev>

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const filters = require('./eleventy-config/filters');
-const collections = require('./eleventy-config/collections')
+const collections = require('./eleventy-config/collections');
+const data = require('./eleventy-config/data');
 const { options } = require('./eleventy-config/options');
 const eleventySass = require("@grimlink/eleventy-plugin-sass");
 const sass = require("sass");
@@ -11,6 +12,10 @@ module.exports = function(eleventyConfig) {
 
 	Object.keys(collections).forEach((name) => {
 		eleventyConfig.addCollection(name, collections[name]);
+	});
+
+	Object.keys(data).forEach((name) => {
+		eleventyConfig.addGlobalData(name, data[name]);
 	});
 
 	eleventyConfig.addPlugin(eleventySass, { 
